581f3f503a62b8035d8b9ca058afa7f39691b2e7
HelloShaohua <wu.shaohua@foxmail.com>
2021-12-04 19:51:01 +0800

    feat: add redis connector (#5)

--- go.mod
+++ go.mod
@@ -3,13 +3,16 @@ module github.com/coolstina/connecter
 go 1.16
 
 require (
-	github.com/coolstina/fsfire v1.0.5 // indirect
-	github.com/coolstina/mosso v1.0.0 // indirect
+	github.com/coolstina/fsfire v1.0.5
+	github.com/coolstina/mosso v1.0.0
 	github.com/gabriel-vasile/mimetype v1.4.0 // indirect
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/go-redis/redis/v8 v8.11.4 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
-	github.com/olivere/elastic v6.2.37+incompatible // indirect
+	github.com/olivere/elastic v6.2.37+incompatible
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/stretchr/testify v1.7.0
+	go.opentelemetry.io/otel v0.16.0 // indirect
 	golang.org/x/net v0.0.0-20211123203042-d83791d6bcd9 // indirect
 	gorm.io/driver/mysql v1.2.0
 	gorm.io/gorm v1.22.3
---
+++ redis/redis.go
@@ -0,0 +1,99 @@
+// Copyright 2021 helloshaohua <wu.shaohua@foxmail.com>;
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package redis
+
+import (
+	"reflect"
+	"time"
+
+	"github.com/go-redis/redis"
+)
+
+// NewConnection create a new gorm db instance with the given options.
+func NewConnection(config *Config, ops ...Option) (*redis.Client, error) {
+	configure := configuration(config)
+
+	for _, o := range ops {
+		o.apply(configure)
+	}
+
+	return redis.NewClient(options(configure)), nil
+}
+
+func configuration(config *Config) *Config {
+	configure := &Config{
+		Network:            "tcp",
+		Host:               config.Host,
+		Password:           config.Password,
+		Database:           config.Database,
+		MinRetryBackoff:    time.Millisecond * 8,
+		MaxRetryBackoff:    time.Millisecond * 512,
+		DialTimeout:        time.Second * 5,
+		ReadTimeout:        time.Second * 3,
+		WriteTimeout:       time.Second * 3,
+		MinIdleConns:       10,
+		MaxConnAge:         time.Second * 3600 * 8, // 8 hours.
+		PoolTimeout:        time.Second * 4,
+		IdleTimeout:        time.Minute * 5,
+		IdleCheckFrequency: time.Minute,
+	}
+	return configure
+}
+
+func options(configure *Config) *redis.Options {
+	options := &redis.Options{}
+	ov := reflect.ValueOf(options).Elem()
+	ct := reflect.TypeOf(configure).Elem()
+	cv := reflect.ValueOf(configure).Elem()
+
+	for i := 0; i < cv.NumField(); i++ {
+		if cv.Field(i).IsValid() {
+			switch ct.Field(i).Name {
+			case "Host":
+				value := ov.FieldByName("Addr")
+				if value.IsValid() && value.CanSet() {
+					if value.Kind() == reflect.String {
+						value.SetString(cv.Field(i).String())
+					}
+				}
+			case "Database":
+				value := ov.FieldByName("DB")
+				if value.IsValid() && value.CanSet() {
+					if value.Kind() == reflect.Int {
+						value.SetInt(cv.Field(i).Int())
+					}
+				}
+			default:
+				if !cv.Field(i).IsZero() {
+					field := ov.FieldByName(ct.Field(i).Name)
+					if field.IsValid() && field.CanSet() {
+						if field.Kind() == reflect.String {
+							field.SetString(cv.Field(i).String())
+						}
+
+						if field.Kind() == reflect.Int64 {
+							field.SetInt(cv.Field(i).Int())
+						}
+
+						if field.Kind() == reflect.Int {
+							field.SetInt(cv.Field(i).Int())
+						}
+					}
+				}
+			}
+		}
+	}
+	return options
+}
